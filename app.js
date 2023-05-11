a3b3a5577cd1b864210f2c2cd924a18376f2004a
SebasBonilla16 <sebastianbonillaleal@gmail.com>
2023-05-11 14:13:49 -0400

    fixed broken hamburger on contact and updated shop page media query

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 //Hamburger Menu
-let menu = document.querySelector('#menu-icon');
+// uses DOM to search for 1st element that matches CSS selector
+let menu = document.querySelector('#menu-icon'); 
 let navbar = document.querySelector('.navbar');
 
+// Grabs menu element from html and toggles the hamburger menu and navbar
 menu.onclick = () => {
   menu.classList.toggle(`bx-x`);
   navbar.classList.toggle(`active`);
 };
-
+// When scroll on page, hamburger menu is removed
 window.onscroll = () => {
   menu.classList.remove('bx-x');
   navbar.classList.remove('active');
@@ -27,7 +29,7 @@ function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
+//Image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
@@ -36,11 +38,12 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
+  if (n > slides.length) {slideIndex = 1} //Checks if the slide index is out of bounds, wraps around to the first slide
+  if (n < 1) {slideIndex = slides.length} //Wrap around to the last slide
+  for (i = 0; i < slides.length; i++) { //hide all slides
     slides[i].style.display = "none";
   }
+  //Displays the current slide and mark corresponding dot as active
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
