45f513c62cffef9937d58f6ab99d561ca85513f8
SebasBonilla16 <sebastianbonillaleal@gmail.com>
2023-05-11 14:15:05 -0400

    fixed hamburger

--- shop.js
+++ shop.js
@@ -1,15 +1,20 @@
 // JSON
 let http = new XMLHttpRequest();
-
+//Open a GET request to get my products.json 
 http.open('get', 'products.json', true);
-
+//Sends the request
 http.send();
 
 http.onload = function(){
+    //Checks if the request in the readyState is DONE and the status is OK
     if(this.readyState == 4 && this.status == 200){
+        // Parse the response text as JSON and store it in 'products' variable
         let products = JSON.parse(this.responseText);
+        //Empty String to store generated HTML
         let output = "";
+        //Iterate over each item in the 'products' array
         for(let item of products){
+            // Generate HTML code for each product item and connect it to JSON
             output += `
                 <div class="products">
                     <img src="${item.image}" alt="${item.image}">
@@ -20,8 +25,9 @@ http.onload = function(){
                     </p>
                     <p class="cart"> Add to cart <i class="fa-solid fa-cart-shopping"></i></p>
                 </div>
-                `;
+                `; 
         }
+        // Set the HTML content of the element with the class "products" to the generated 'output' string
         document.querySelector(".products").innerHTML = output;
     }
 }
